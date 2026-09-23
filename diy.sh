6faeed095cb88db15e8fb2ffdcbc72e04b4ec525
bg4vni <72649376+bg4vni@users.noreply.github.com>
2026-09-23 15:24:13 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -33,6 +33,16 @@ EOF
 #将默认IP修改为192.168.100.1
 sed -i 's/192.168.1.1/192.168.100.1/g' openwrt/package/base-files/files/bin/config_generate
 
+# 1. 添加 Nikki 官方软件源
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
+
+# 2. 添加 PassWall 2 官方源（通过 feeds 机制拉取，会自动匹配系统依赖）
+echo 'src-git passwall_dep https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >> feeds.conf.default
+
+# 3. 添加luci-adguardhome软件源
+echo 'src-git adguardhome https://github.com/kenzok8/openwrt-packages.git;main' >> feeds.conf.default
+
 #cat >> openwrt/feeds.conf.default <<'EOF'
 #src-git openclash https://github.com/vernesong/OpenClash.git
 #src-git ddnsto https://github.com/linkease/ddnsto-openwrt.git
